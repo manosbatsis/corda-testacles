1ec6d736267a2ea209ef3e876ab7af412aa19d74
Manos Batsis <manosbatsis@gmail.com>
2020-12-03 23:30:25 +0200

    CI test

--- corda-testacles-cordapp-example/src/test/kotlin/testacles/sample/test/containers/cordform/CordformNetworkContainerTest.kt
+++ corda-testacles-cordapp-example/src/test/kotlin/testacles/sample/test/containers/cordform/CordformNetworkContainerTest.kt
@@ -22,6 +22,7 @@
 package testacles.sample.test.containers.cordform
 
 import com.github.manosbatsis.corda.testacles.containers.cordform.CordformNetworkContainer
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Tags
 import org.slf4j.LoggerFactory
@@ -36,6 +37,7 @@ import testacles.sample.test.containers.cordform.base.Util.createCordformNetwork
 /** An RPC-based test using [CordformNetworkContainer] */
 @Testcontainers
 @Tags(Tag("cordform"))
+@Disabled
 class CordformNetworkContainerTest : CordformNetworkContainerTestBase(){
 
     companion object {
