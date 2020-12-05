eece0481eaffb30488b93fc3f5b0056beae9f339
Manos Batsis <manosbatsis@gmail.com>
2020-12-05 16:11:30 +0200

    CI test

---
+++ prepare-ci-artifacts.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+find . -name "*.bak" -type f -delete
+
+
+for cordaUseVersion in "4.5" "4.6"; do
+  echo "Run Gradle build with Corda version $cordaUseVersion"
+  if ! ./gradlew clean build -x dokkaJavadoc "-PcordaVariationVersion=$cordaUseVersion" --no-daemon; then
+    echo "Build for $cordaUseVersion failed, script exit with error."
+    exit 1
+  else
+    echo "Build for $cordaUseVersion passed."
+  fi
+done
